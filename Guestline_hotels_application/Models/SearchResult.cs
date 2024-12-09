900ff934bd14b6a59cfc7be316dfc72b629d7710
pawel <zabkooo@gmail.com>
2024-12-09 10:15:14 +0100

    SearchResult fix

--- Guestline_hotels_application/Models/SearchResult.cs
+++ Guestline_hotels_application/Models/SearchResult.cs
@@ -1,10 +1,8 @@
-﻿namespace Guestline_hotels_application.Models
-{
-	public class SearchResult
-	{
-		public DateTime StartDate { get; init; }
-		public DateTime EndDate { get; init; }
-		public int Availability { get; init; }
-	}
-}
+﻿namespace Guestline_hotels_application.Models;
 
+public class SearchResult
+{
+	public DateTime StartDate { get; init; }
+	public DateTime EndDate { get; init; }
+	public int Availability { get; init; }
+}
